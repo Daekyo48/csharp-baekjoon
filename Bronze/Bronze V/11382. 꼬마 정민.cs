2270a029badfe047e6001660fccbec4bf4db8ae5
Daekyo48 <karasin1013@gmail.com>
2025-04-05 16:42:33 +0900

    Add 11382.cs

---
+++ Bronze/Bronze V/11382. 꼬마 정민.cs
@@ -0,0 +1,13 @@
+class Class
+{
+    static void Main()
+    {
+        string[] numbers = Console.ReadLine()!.Split();
+
+        long a = long.Parse(numbers[0]);
+        long b = long.Parse(numbers[1]);
+        long c = long.Parse(numbers[2]);
+
+        Console.WriteLine(a + b + c);
+    }
+}

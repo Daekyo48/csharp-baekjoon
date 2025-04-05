accb0706afbbaa6862c8f06cba694ece2bd79c9e
Daekyo48 <karasin1013@gmail.com>
2025-04-05 16:24:51 +0900

    Add 01008.cs

---
+++ Bronze/Bronze V/01008. A／B.cs
@@ -0,0 +1,12 @@
+class Class
+{
+    static void Main()
+    {
+        string[] numbers = Console.ReadLine()!.Split();
+
+        double a = int.Parse(numbers[0]);
+        double b = int.Parse(numbers[1]);
+
+        Console.WriteLine(a / b);
+    }
+}

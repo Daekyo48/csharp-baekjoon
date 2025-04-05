72850ed15ef654b2fc76866bff597bccd8f07e4f
Daekyo48 <karasin1013@gmail.com>
2025-04-05 16:25:28 +0900

    Add 10430.cs

---
+++ Bronze/Bronze V/10430. 나머지.cs
@@ -0,0 +1,16 @@
+class Class
+{
+    static void Main()
+    {
+        string[] numbers = Console.ReadLine()!.Split();
+
+        int a = int.Parse(numbers[0]);
+        int b = int.Parse(numbers[1]);
+        int c = int.Parse(numbers[2]);
+        
+        Console.WriteLine((a + b) % c);
+        Console.WriteLine(((a % c) + (b % c)) % c);
+        Console.WriteLine((a * b) % c);
+        Console.WriteLine(((a % c) * (b % c)) % c);
+    }
+}

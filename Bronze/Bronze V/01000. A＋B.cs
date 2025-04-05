0650844acf20a31f2599539444b90872d922ffd8
Daekyo48 <karasin1013@gmail.com>
2025-04-05 16:24:28 +0900

    Add 01000.cs

---
+++ Bronze/Bronze V/01000. A＋B.cs
@@ -0,0 +1,12 @@
+class Class
+{
+    static void Main()
+    {
+        string[] numbers = Console.ReadLine()!.Split();
+
+        int a = int.Parse(numbers[0]);
+        int b = int.Parse(numbers[1]);
+
+        Console.WriteLine(a + b);
+    }
+}

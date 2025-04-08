3439a8fbf8a9fdb731506bb63733c66358a586af
Daekyo48 <karasin1013@gmail.com>
2025-04-08 20:48:13 +0900

    Add 10813.cs

---
+++ Bronze/Bronze II/10813. 공 바꾸기.cs
@@ -0,0 +1,23 @@
+class Class
+{
+    static void Main()
+    {
+        string[] values = Console.ReadLine()!.Split();
+
+        int n = int.Parse(values[0]);
+        int m = int.Parse(values[1]);
+        int[] baskets = Enumerable.Range(1, n).ToArray();
+
+        for (int index = 0; index < m; index++)
+        {
+            string[] ways = Console.ReadLine()!.Split();
+
+            int i = int.Parse(ways[0]) - 1;
+            int j = int.Parse(ways[1]) - 1;
+
+            (baskets[i], baskets[j]) = (baskets[j], baskets[i]);
+        }
+
+        Console.WriteLine(string.Join(' ', baskets));
+    }
+}

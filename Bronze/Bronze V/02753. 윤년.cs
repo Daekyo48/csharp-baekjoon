f9d66fbd4109a54db7c3a244591d7f2d1f531c68
Daekyo48 <karasin1013@gmail.com>
2025-04-08 20:45:42 +0900

    Add 02753.cs

---
+++ Bronze/Bronze V/02753. 윤년.cs
@@ -0,0 +1,9 @@
+class Class
+{
+    static void Main()
+    {
+        int year = int.Parse(Console.ReadLine()!);
+
+        Console.WriteLine(year % 4 == 0 && year % 100 != 0 || year % 400 == 0 ? 1 : 0);
+    }
+}
